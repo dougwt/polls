e36cae02f3708fbb32f86ad505cf8ca402c456d2
Doug Tyler <dougwt@gmail.com>
2018-05-24 20:20:59 -0700

    Add tests for action creator

---
+++ client/src/actions/index.test.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import * as actions from './index';
+import * as types from './types';
+
+describe('actions', () => {
+  it('creates an action to fetch the currently auth\'d user', () => {
+    // Mock axios request to `/api/current_user
+    const mock = new MockAdapter(axios);
+    mock.onGet('/api/current_user').reply(200, 'PAYLOAD');
+
+    const expectedAction = { type: types.FETCH_USER, payload: 'PAYLOAD' }
+
+    // Process async action creator
+    const dispatch = jest.fn();
+    actions.fetchUser()(dispatch)
+      .then(() => dispatch.mock.calls[0][0])
+      .then(action => {
+        expect(action).toEqual(expectedAction);
+      })
+  })
+});
