6528276bb6aff896093bc1a9db359b60c19a7189
Doug Tyler <dougwt@gmail.com>
2018-05-31 14:04:10 -0700

    Add additional tests for PollForm component

--- client/src/components/polls/PollForm.test.js
+++ client/src/components/polls/PollForm.test.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { shallow, mount } from 'enzyme';
 import { Provider } from 'react-redux';
 import { reducer as formReducer } from 'redux-form';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import { StaticRouter } from 'react-router';
-
 import ReduxForm, { PollForm } from './PollForm';
 
 let props;
@@ -28,6 +28,11 @@ describe('PollForm', () => {
     expect(wrapper.find('form').exists()).toBe(true);
   });
 
+  // TODO: Add owner field to form as a hidden field
+  // xit('shows a hidden `Owner` field', () => {
+  //   expect(wrapper.find('input[name="owner"]').exists()).toBe(true);
+  // });
+
   it('should ask for a question', () => {
     expect(wrapper.find('Field[name="question"]').exists()).toBe(true);
   });
@@ -44,13 +49,81 @@ describe('PollForm', () => {
     expect(wrapper.find('.btn-cancel').exists()).toBe(true);
   });
 
-  it('submits the form when Preview button is clicked', () => {
-    wrapper.find('.btn-preview').simulate('click');
-    expect(props.handleSubmit).toHaveBeenCalled();
+  describe('when the `Preview` button is clicked', () => {
+    let store;
+
+    beforeEach(() => {
+      store = createStore(
+        combineReducers({ form: formReducer }),
+        applyMiddleware(thunk)
+      );
+      const initialValues = { choices: [0, 1] };
+
+      wrapper = mount(
+        <Provider store={store}>
+          <StaticRouter context={{}}>
+            <ReduxForm {...props} initialValues={initialValues} />
+          </StaticRouter>
+        </Provider>
+      );
+    });
+
+    it('submits the form', () => {
+      wrapper.find('Button.btn-preview').simulate('click');
+      expect(props.handleSubmit).toHaveBeenCalled();
+    });
+
+    describe('when the form validation fails', () => {
+      beforeEach(() => {
+        wrapper.find('input[name="question"]').simulate('change', {
+          target: { value: 'Would you like to play a game?' }
+        });
+        wrapper.find('Button.btn-preview').simulate('click');
+        expect(props.handleSubmit).toHaveBeenCalled();
+      });
+
+      it('shows an error', () => {
+        const form = store.getState().form.pollForm;
+        expect(form.values.question).toBe('Would you like to play a game?');
+        expect(form.syncErrors.choice_1).toBe(
+          'You must provide text for Choice 1'
+        );
+        expect(form.syncErrors.choice_2).toBe(
+          'You must provide text for Choice 2'
+        );
+      });
+    });
+
+    describe('when the form validation succeeds', () => {
+      beforeEach(() => {
+        wrapper.find('input[name="question"]').simulate('change', {
+          target: { value: 'Would you like to play a game?' }
+        });
+        wrapper
+          .find('input[name="choice_1"]')
+          .simulate('change', { target: { value: 'Yes' } });
+        wrapper
+          .find('input[name="choice_2"]')
+          .simulate('change', { target: { value: 'No' } });
+        wrapper.find('Button.btn-preview').simulate('click');
+        expect(props.handleSubmit).toHaveBeenCalled();
+      });
+
+      // TODO: Add a test to confirm user is shown the next component
+      it('does not show any errors', () => {
+        const form = store.getState().form.pollForm;
+        expect(form.values.question).toBe('Would you like to play a game?');
+        expect(form.values.choice_1).toBe('Yes');
+        expect(form.values.choice_2).toBe('No');
+        expect(form.syncErrors).toBeUndefined();
+      });
+    });
   });
 
-  it('returns to the Dashboard when Back button is clicked', () => {
-    expect(wrapper.find('.btn-cancel').props().to).toEqual('/polls');
+  describe('when the `Cancel` button is clicked', () => {
+    it('returns to the Dashboard when Back button is clicked', () => {
+      expect(wrapper.find('.btn-cancel').props().to).toEqual('/polls');
+    });
   });
 
   describe('renderChoices', () => {
