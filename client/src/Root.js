d27083335365f0b2385d8116127ad9cc682c4e80
Doug Tyler <dougwt@gmail.com>
2018-06-11 22:39:37 -0700

    Cleanup Root.js

--- client/src/Root.js
+++ client/src/Root.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import reduxThunk from 'redux-thunk';
 import reducers from './reducers';
 
-export default function wrapWithProvider({ children, initialState = {} }) {
+const wrapWithProvider = ({ children, initialState = {} }) => {
   const store = createStore(
     reducers,
-    {},
+    initialState,
     composeWithDevTools(applyMiddleware(reduxThunk))
   );
 
   return <Provider store={store}>{children}</Provider>;
-}
+};
+wrapWithProvider.propTypes = {
+  children: PropTypes.object.isRequired,
+  initialState: PropTypes.object
+};
+
+export default wrapWithProvider;
