8fd65e133801264c845673b0312f8f3df0c93572
Doug Tyler <dougwt@gmail.com>
2018-11-09 13:03:47 -0800

    Fix failing test

--- controllers/poll_controller.js
+++ controllers/poll_controller.js
@@ -32,7 +32,9 @@ module.exports = {
 
         // Update the respondents list
         User.findById(userId).then(user => {
-          results = poll.respondents.filter(responder => responder === userId);
+          results = poll.respondents.filter(responder =>
+            responder.equals(userId)
+          );
           // Reject if userId has already voted on this poll
           if (results.length > 0) {
             return res
