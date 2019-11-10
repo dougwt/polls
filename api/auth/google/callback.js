8ff72a79d0b45303891d6141eb56923243836c34
Doug Tyler <dougwt@gmail.com>
2019-11-10 13:35:14 -0800

    Add withLogger middleware to google auth routes

--- api/auth/google/callback.js
+++ api/auth/google/callback.js
@@ -1,12 +1,16 @@
 const { applyMiddleware } = require('micro-mw');
 const passport = require('passport');
+const logger = require('../../../lib/logger');
+const withLogger = require('../../../lib/withLogger');
 const withPassport = require('../../../lib/withPassport');
 const withMongoose = require('../../../lib/withMongoose');
-const logger = require('../../../lib/logger');
 
-module.exports = applyMiddleware([withMongoose, withPassport], (req, res) => {
-  logger.debug('you reached the callback URI', { id: req.id });
-  passport.authenticate('google')(req, res, () => {
-    res.redirect('/polls');
-  });
-});
+module.exports = applyMiddleware(
+  [withLogger, withMongoose, withPassport],
+  (req, res) => {
+    logger.debug('you reached the callback URI', { id: req.id });
+    passport.authenticate('google')(req, res, () => {
+      res.redirect('/polls');
+    });
+  }
+);
--- api/auth/google/index.js
+++ api/auth/google/index.js
@@ -1,11 +1,15 @@
 const { applyMiddleware } = require('micro-mw');
 const passport = require('passport');
+const logger = require('../../../lib/logger');
+const withLogger = require('../../../lib/withLogger');
 const withPassport = require('../../../lib/withPassport');
 const withMongoose = require('../../../lib/withMongoose');
-const logger = require('../../../lib/logger');
 
-module.exports = applyMiddleware([withMongoose, withPassport], (req, res) => {
-  passport.authenticate('google')(req, res, (...args) => {
-    logger.debug('passport authenticated', args);
-  });
-});
+module.exports = applyMiddleware(
+  [withLogger, withMongoose, withPassport],
+  (req, res) => {
+    passport.authenticate('google')(req, res, (...args) => {
+      logger.debug('passport authenticated', args);
+    });
+  }
+);
